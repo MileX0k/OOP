13b4cb4a8c1722901f32b753ecbae9bc550681f1
MileX0k <81148586+MileX0k@users.noreply.github.com>
2021-08-05 03:20:31 +0500

    Add files via upload

---
+++ Lesson 6.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+//===========================================TASK 1================================================
+template<class T>
+T Task1()
+{
+	T a;
+
+	cout << "Enter your number: "; 
+	cin >> a;
+
+	while (cin.fail() || cin.get() != '\n')
+	{
+		cin.clear();
+		cin.ignore(9999, '\n');
+		cin.sync();
+		cout << "Error! Wrong number!\n\n" << "Enter your number: ";
+		cin >> a;
+	}
+	return a;
+}
+
+
+
+int main()
+{
+//===========================================TASK 3================================================	
+	cout << "   TASK 1" << endl;
+
+	while (true)
+	{
+		Task1<int>();
+		break;
+	}
+
+	cout << endl;
+
+
+
+	return 0;
+}
